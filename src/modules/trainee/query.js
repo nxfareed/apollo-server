a0aaa1d8dfdda7e601a07fe713c37461ba82bc2d
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-06-25 13:32:52 +0530

    feat: Mutation in GraphQl

---
+++ src/modules/trainee/query.js
@@ -0,0 +1,13 @@
+import userInstance from "../../service/user";
+
+export default {
+  getAllTrainees: () => {
+    return userInstance.getAllUser();
+  },
+  getTrainee: (parent, args, context) => {
+    const {
+      id
+    } = args;
+    return userInstance.getUser(id);
+  }
+};
